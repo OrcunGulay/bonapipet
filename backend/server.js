509f5bc3cc1d2ebc43b2680bb75c2857e4438d00
Orçun Gülay <tsts4695@gmail.com>
2026-06-05 19:48:11 +0300

    server updated

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ const adminRoutes = require('./routes/adminRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
 // ── Güvenlik Middleware (Helmet'i Cross-Origin ile yapılandır) ──────────────
 // crossOriginResourcePolicy: false yaparak resimlerin dışarıdan yüklenmesini engellemiyoruz
 app.use(helmet({
